cf25e3d867d0e8b2b8524b251b686b267aab1a12
tonyali <tony.ali@ynov.com>
2021-01-12 10:15:55 +0100

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+function tache() {
+    var item = document.getElementById("input").value
+    var text = document.createTextNode(item)
+    var newItem = document.createElement("li")
+    newItem.appendChild(text)
+    document.getElementById("tache").appendChild(newItem)
+}
